c5e982cc1a7651538b1da63681875d7316f3e17e
SeanTimothy <472942998@tqstudent.edu.au>
2023-01-08 13:10:54 +1000

    Version 0.47

--- src/crazy.js
+++ src/crazy.js
@@ -3,3 +3,4 @@ function load_html_files( ){
     $("#crazyheader").load("https://seantimothy.github.io/portfolio/html/header.html"); 
     $("#crazyfooter").load("https://seantimothy.github.io/portfolio/html/footer.html"); 
 }
+ 
